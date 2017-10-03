98a73b4b1ecfb217f44a82342d308777a2aed0fa
Devin Cenatiempo <dcenatiempo@gmail.com>
2017-10-03 17:45:14 -0600

    Updated nowServing.cpp/.h Passing all 5 testbeds!

--- nowServing.cpp
+++ nowServing.cpp
@@ -14,6 +14,19 @@
 #include "nowServing.h" // for nowServing() prototype
 #include "deque.h"      // for DEQUE
 using namespace std;
+using namespace custom;
+
+void display(Request current){
+   //Currently serving Sam for class CS124. Time left: 1
+   //Emergency for Steve for class CS124. Time left: 2
+   if (current.isEmergency())
+      cout << "\tEmergency for ";
+   else
+      cout << "\tCurrently serving ";
+   cout << current.getName() << " for class "
+   << current.getCourse() << ". Time left: "
+   << current.getMinute() << endl;
+}
 
 /************************************************
  * NOW SERVING
@@ -30,7 +43,64 @@ void nowServing()
    cout << "\tfinished                     : end simulation\n";
 
    // your code here
-
+   string course;
+   string name;
+   int minutes;
+   string instruction;
+   int time = 0;
+   
+   deque <Request> labLine(4);
+   Request current;
+   
+   try
+   {
+      do
+      {
+         cout << "<" << time << "> " ;
+         cin  >> instruction;
+         if (instruction == "none")
+         {
+            // no input, 1 minute passes
+         }
+         else if (instruction == "finished")
+         {
+            break;
+         }
+         else if (instruction == "!!")
+         {
+            //need to add validation/error handling
+            cin >> course;
+            cin >> name;
+            cin >> minutes;
+            // add new request to the back of the deque
+            labLine.push_front(Request(course, name, minutes, 1));
+         }
+         else
+         {
+            cin >> name;
+            cin >> minutes;
+            // add new request to the back of the deque
+            labLine.push_back(Request(instruction, name, minutes, 0));
+         }
+         if ( current.getMinute() == 0 && !labLine.empty() )
+         {
+            current = labLine.front();
+            labLine.pop_front();
+         }
+         if (current.getMinute())
+         {
+            display(current);
+            current.subtractMinute();
+         }
+         time++;
+      }
+      while (instruction != "finished");
+   }
+   catch (const char * error)
+   {
+      cout << error << endl;
+   }
+   
    // end
    cout << "End of simulation\n";
 }
--- nowServing.h
+++ nowServing.h
@@ -11,10 +11,33 @@
 #ifndef NOW_SERVING_H
 #define NOW_SERVING_H
 
-#include "deque.h"     // for DEQUE
+//#include "deque.h"     // for DEQUE
+#include <string>
+
+using namespace std;
 
 // the interactive nowServing program
 void nowServing();
+
+class Request
+{
+private:
+   string course;
+   string name;
+   int minutes;
+   bool emergency;
+public:
+   Request() {course = ""; name = ""; minutes = 0; emergency = 0; }
+   Request( string course, string name, int minutes, bool emergency )
+   { this->course = course; this->name = name; this->minutes = minutes; this->emergency = emergency; }
+   
+   string getCourse() { return course; }
+   string getName() { return name; }
+   int getMinute() { return minutes; }
+   void subtractMinute() { minutes--; }
+   bool isEmergency() { return emergency; }
+};
+
 
 #endif // NOW_SERVING_H
 
